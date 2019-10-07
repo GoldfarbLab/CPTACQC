306e751ee0cceaef93abe85d750d09e960623a69
Ria Jasuja <riajasuja@Goldfarbs-iMac.local>
2019-10-07 15:39:55 -0500

    working on labelling efficiency table

--- R/LabelingQC.R
+++ R/LabelingQC.R
@@ -29,6 +29,17 @@ calc_labeling_efficiency <- function(filtered.data)
   #calculate N term label efficiency
   nterm.labelling.efficiency <- sum(filtered.data$detected_nterm)/sum(filtered.data$expected_nterm)
 
+  # num_fully_labeled, num_partially_labeled, num_unlabeled, num_no_sites_available, num_overlabeled,
+  num.fully.labeled <- sum(filtered.data$labelling_efficiency == "Fully Labelled")
+  print(num.fully.labeled)
+  num.partially.labeled <- sum(filtered.data$labelling_efficiency == "Partially Labelled")
+  print(num.partially.labeled)
+  num.unlabeled <- sum(filtered.data$labelling_efficiency == "Unlabelled")
+  print(num.unlabeled)
+  #percent_fully_labeled, percent_partially_labeled, percent_unlabeled, percent_no_sites_available, percent_overlabeled,
+
+  #num_lys_labeled, num_lys_unlabeled, num_lys_blocked, num_nterm_labeled, num_nterm_unlabeled, num_nterm_blocked
+
   #create a tibble with these three values inside of it
   efficiency.calculations <- tibble("Labelling Efficiency" = calculate.labelling.efficiency, "Lysine Labelling Efficiency" = lysine.labelling.efficiency, "N term Labelling Efficiency" = nterm.labelling.efficiency)
   efficiency.calculations
